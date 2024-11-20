8258ba40b5f9f9cc3420ce960261692747cbf94c
dnlaylo <laylomarga@gmail.com>
2024-11-20 20:52:59 +0800

    Insert to loop to be able to ask for new inputs

--- collect_personal_info.py
+++ collect_personal_info.py
@@ -1,17 +1,24 @@
 # Open file and let the existing file append inputs and not override by using 'a'
 with open("personal_info.txt", "a") as file:
-    # Ask for entries (name, age, address, contact no., email)
-    full_name = input("Full Name: ")
-    age = int(input("Age: "))
-    contact = input("Contact Number: ")
-    address = input("Address: ")
-    email = input("Email: ")
+    while True:
+        # Ask for entries (name, age, address, contact no., email)
+        full_name = input("Full Name: ")
+        age = int(input("Age: "))
+        contact = input("Contact Number: ")
+        address = input("Address: ")
+        email = input("Email: ")
 
-# Write the inputs to the file respectively
-    file.write(f"Full Name: {full_name}\n")
-    file.write(f"Age: {age}\n")
-    file.write(f"Contact Number: {contact}\n")
-    file.write(f"Address: {address}\n")
-    file.write(f"Email: {email}\n")
-    
-# Ask for a new input
+        # Write the inputs to the file respectively
+        file.write(f"Full Name: {full_name}\n")
+        file.write(f"Age: {age}\n")
+        file.write(f"Contact Number: {contact}\n")
+        file.write(f"Address: {address}\n")
+        file.write(f"Email: {email}\n")
+
+        # Ask for a new input
+        another = input("\nDo you want to add another person? (yes/no): ").lower()
+        if another == "yes":
+            continue
+        elif another == "no":
+            print(f"\nData saved.")
+            break
