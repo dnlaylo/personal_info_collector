3765aa4f5208bdfd3699218294c1762b55747117
dnlaylo <laylomarga@gmail.com>
2024-11-20 22:26:00 +0800

    Using a loop to go through the data. Finding is successful but does not print every info

--- search_personal_info.py
+++ search_personal_info.py
@@ -3,7 +3,13 @@ with open("personal_info.txt", "r") as file:
     # Create a variable to read lines in the file
     lines = file.readlines()
 
+while True:
     # Ask for a name input and remove extra spaces
     fullname = input("\nEnter the full name to search: ").strip()
 
 # Find the respective name input in the file
+    for line in lines:
+        if line.strip() == f"Full Name: {fullname}":
+            if line.strip() == "-----":
+                break
+            print('\n', line.strip())
