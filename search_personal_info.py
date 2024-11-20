5f47f305700df7e58c673134f9eef39c19f9ac74
dnlaylo <laylomarga@gmail.com>
2024-11-20 22:44:10 +0800

    False value for found and inserted the current program for searching into for loop which loops until separator line is reached

--- search_personal_info.py
+++ search_personal_info.py
@@ -6,10 +6,17 @@ with open("personal_info.txt", "r") as file:
 while True:
     # Ask for a name input and remove extra spaces
     fullname = input("\nEnter the full name to search: ").strip()
+    # Initial value for 'found'
+    found = False
 
-# Find the respective name input in the file
+    # Find the respective name input in the file
     for line in lines:
         if line.strip() == f"Full Name: {fullname}":
-            if line.strip() == "-----":
+            # If name input matches data from file, then 'foun' becomes true
+            found = True
+
+        # found is set to True and prints lines until it reaches the separator line
+        if found:
+            if line.strip() == "-----": # Separator line
                 break
             print('\n', line.strip())
