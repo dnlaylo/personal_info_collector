f9a5e627aece17b6bfd9c63cbbdf5c2dfe33d6cc
dnlaylo <laylomarga@gmail.com>
2024-11-20 21:28:50 +0800

    Open file, read lines variable, ask for input

--- search_personal_info.py
+++ search_personal_info.py
@@ -1,4 +1,9 @@
 # Open file and let it read
-# Create a variable to read lines in the file
-# Ask for a name input
+with open("personal_info.txt", "r") as file:
+    # Create a variable to read lines in the file
+    lines = file.readlines()
+
+    # Ask for a name input and remove extra spaces
+    fullname = input("\nEnter the full name to search: ").strip()
+
 # Find the respective name input in the file
