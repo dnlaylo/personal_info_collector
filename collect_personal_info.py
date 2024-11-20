74eb7f2857af5d2193e04b2255ca78f21fef2fb6
dnlaylo <laylomarga@gmail.com>
2024-11-20 19:19:11 +0800

    Pseudocode

---
+++ collect_personal_info.py
@@ -0,0 +1,4 @@
+# Open file and let the existing file append inputs and not override by using 'a'
+# Ask for entries (name, age, address, contact no., email)
+# Write the inputs to the file respectively
+# Ask for a new input
